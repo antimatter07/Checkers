a38ef0317539160392322ce92b11a17854983569
Matthew Villarica <matthewvillarica@gmail.com>
2021-12-22 13:52:26 +0800

    getters, setters, board initialization with pieces, for testing

--- Board.java
+++ Board.java
@@ -1,17 +1,70 @@
 public class Board {
     Square[][] board;
+    private int dimension;
 
+    /**
+     * This constructor isntantiates the board, which is the initial
+     * set up of pieces in standard checkers on an 8 x 8 board.
+     * 
+     * @param dim The dimension of the board, usually 8 but can be changed
+     *            for testing
+     */
     public Board(int dim) {
-        board = new Square[8][8];
+        dimension = dim;
+        board = new Square[dimension][dimension];
+        PlayerSide cur_side = PlayerSide.COMPUTER;
 
-        for(int i = 0; i < dim; i++) {
+        for(int i = 0; i < dimension; i++) {
 
-            for(int j = 0; j < dim; j++) {
+            if(i > 2)
+                cur_side = PlayerSide.HUMAN;
 
-                
+            for(int j = 0; j < dimension; j++) {
+
+                if(i == 0 || i == 2 || i == 6) {
+                    if(j % 2 != 0) {
+                        board[i][j] = new Square(i, j, new Piece(cur_side, i, j));
+                    }
+                } else if (i == 1 || i == 5 || i == 7) {
+                    if(j % 2 == 0) {
+                        board[i][j] = new Square(i, j, new Piece(cur_side, i, j));
+                    }
+                } else {
+                    board[i][j] = new Square(i, j);
+                }
+
+
             }
         }
 
     }
     
+    /**
+     * This method displays the board in the console.
+     */
+    public void display() {
+        for(int i =0; i < dimension; i++){
+           
+            for(int j =0; j < dimension; j++){
+                System.out.print("----");
+            }
+            System.out.println("-");
+            
+            for(int j =0; j < dimension; j++){
+                if(board[i][j].getPiece() != null){
+                  System.out.print("| " + board[i][j].getPiece().getSymbol() + " ");
+                }else{
+                  System.out.print("|   ");   
+                }
+
+            }
+            System.out.println("|");
+        }
+        for(int i =0; i < dimension; i++){       
+            System.out.print("----");
+        }
+        System.out.print("-");
+    }
+
 }
+    
--- Square.java
+++ Square.java
@@ -3,9 +3,23 @@ public class Square {
     int row;
     int col;
 
-    public Square(int row, int col) {
+    public Square(int row, int col, Piece piece) {
         this.row = row;
         this.col = col;
+
+        this.piece = piece;
+    }
+
+    public Square(int row, int col) {
+
+        this.row = row;
+        this.col = col;
+
+
+    }
+
+    public Piece getPiece() {
+        return piece;
     }
     
 }
